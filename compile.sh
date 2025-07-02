c8410d5c80e7af384670dcb563134e681fff35a0
Nathan Mentze <nate@nmentz.com>
2025-07-02 18:45:19 -0500

    server/client compile option + debug flags

--- compile.sh
+++ compile.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-gcc client.c -lrdmacm -libverbs -o client
-gcc server.c -lrdmacm -libverbs -o server
+set -e
+
+case "$1" in
+    server)
+        gcc server.c -g -lrdmacm -libverbs -o server
+        ;;
+    client)
+        gcc client.c -g -lrdmacm -libverbs -o client
+        ;;
+    *)
+        echo "Usage: ./compile [client|server]"
+        exit 1
+        ;;
+esac
